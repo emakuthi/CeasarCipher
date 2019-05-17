4301178af83b95ce3cb3c5204461c3f4a596f56c
emakuthi <emakuthi@gmail.com>
2019-05-17 11:45:47 +0300

    updating the code

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -1,55 +1,44 @@
+
+
+
 public class Encrypt {
-   private char alphabet;
+
    private String plainText;
    private int shiftValue;
 
 
-   public Encrypt(char alphabet, String plainText, int shiftValue){
+   public Encrypt(String plainText, int shiftValue){
+        this.plainText = plainText;
+        this.shiftValue = shiftValue;
 
-       this.setAlphabet(alphabet);
-       this.setShiftValue(shiftValue);
-       this.setPlainText(plainText);
    }
 
-    public char getAlphabet() {
-        return alphabet;
-    }
-
-    public void setAlphabet(char alphabet) {
-        this.alphabet = alphabet;
-    }
 
     public String getPlainText() {
         return plainText;
     }
 
     public void setPlainText(String plainText) {
+
         if ((this.plainText != null) && ((!this.plainText.equals("")) && (this.plainText.matches("^[a-zA-Z]*$"))))
         {
-            this.plainText = plainText;
+           this.plainText = plainText;
         }
-        else
-        {
-            throw new IllegalArgumentException("Please Enter a Plain Text");
-        }
+
     }
 
     public int getShiftValue() {
         return shiftValue;
     }
 
-    public void setShiftValue(int shiftValue) {
-        if(this.shiftValue < 0)
+    public void setShiftValue( int shiftValue) {
+
+
+        if(shiftValue < 0 || shiftValue > 25)
         {
-            throw new IllegalArgumentException("The shift value is not valid");
+
+            this.shiftValue =shiftValue;
         }
-        else if(this.shiftValue > 26)
-        {
-            throw new IllegalArgumentException("The shift value has exceeded the limit ");
-        }
-        else
-        {
-            this.shiftValue=shiftValue;
-        }
+
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,48 @@
+import java.util.*;
 public class Main {
+    public static void main(String args[]) {
 
-    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println(" Input the plaintext message : ");
 
+        String plaintext = sc.nextLine();
+
+        System.out.println(" Enter the value by which each character in the plaintext message gets shifted : ");
+
+        int shift = sc.nextInt();
+
+        Encrypt encrypt = new Encrypt(plaintext, shift);
+
+        encrypt.setShiftValue(shift);
+
+        encrypt.setPlainText(plaintext);
+
+        StringBuilder ciphertext = new StringBuilder();
+
+        for(int i=0; i < encrypt.getPlainText().length();i++)
+        {
+            // Shift one character at a time
+            char alphabet = encrypt.getPlainText().charAt(i);
+
+            // if alphabet lies between a and z
+            if((alphabet >= 'a' && alphabet <= 'z') || (alphabet >= 'A' && alphabet <= 'Z'))
+            {
+                // shift alphabet
+                alphabet = (char) (alphabet + encrypt.getShiftValue());
+            }
+            // if shift alphabet greater than 'z'
+            if(alphabet > 'z') {
+                // reshift to starting position
+                alphabet = (char) (alphabet+'a'-'z'-1);
+            }
+            // if shift alphabet greater than 'Z'
+            else if(alphabet > 'Z' && alphabet < 'a') {
+                //reshift to starting position
+                alphabet = (char) (alphabet+'A'-'Z'-1);
+            }
+            ciphertext.append(alphabet);
+        }
+        System.out.println(" ciphertext : " + ciphertext.toString());
     }
 }
